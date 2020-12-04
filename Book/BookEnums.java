6e75dcb49515c582defaaea3b0c911c066ed0e6a
84436 <thien.bui.84436@gmail.com>
2020-12-04 11:49:28 +0700

    [*+] extended enums; string enums (!)

--- Book/BookEnums.java
+++ Book/BookEnums.java
@@ -1,20 +1,68 @@
 package Book;
 
+/**
+ * Specific constants (and their string representations) for book genre, rating and format.
+ */
+
+// RULES (aka. "What to keep in mind while editing this file")
+// 1. Only append: Keep the order of what's already in here.
+// 2. Punctuate: End ALL lines with a colon, and keep the semicolon in a standalone line.
+
 public class BookEnums {
+    // Book genres (adapted from the subject list in Amazon Advanced Books Search)
     public enum BookGenre {
-        FICTION,
-        NON_FICTION,
+        //#region: Values
+        EDUCATION("Education"),
+        SCIENCE("Science"),
+        REFERENCES("References"),
+        BUSINESS("Business & Finance"),
+        LIFESTYLE("Health & Lifestyle"),
+        BIO("Biography"),
+        RELIGIONS("Religions"),
+        POLITICS("Politics"),
+        CHILDREN("For Children"),
+        ROMANCE("Romance"),
+        FICTION("Fiction & Fantasy"),
+        WIBU("Japanese Literature"),
+        ;
+        //#endregion
+
+        //#region: String enum magic
+        private final String fullname;
+        BookGenre(String fullname) { this.fullname = fullname; }
+        public String toString() { return this.fullname; }
+        //#endregion
     }
     
     public enum BookRating {
-        EVERYONE,
-        KIDS,
-        PRETEEN,
-        R18
+        //#region: Values
+        EVERYONE("Everyone"),
+        CHILDREN("Children"),
+        PRETEEN("Pre-teen"),
+        R18("18+"),
+        ;
+        //#endregion
+
+        //#region: String enum magic
+        private final String fullname;
+        BookRating(String fullname) { this.fullname = fullname; }
+        public String toString() { return this.fullname; }
+        //#endregion
     }
     
     public enum BookFormat {
-        PAPERBACK,
-        EBOOK
+        //#region: Values
+        PAPER("Paperback"),
+        HARD("Hardcover"),
+        EBOOK("Ebook"),
+        AUDIO("Audiobook"),
+        ;
+        //#endregion
+
+        //#region: String enum magic
+        private final String fullname;
+        BookFormat(String fullname) { this.fullname = fullname; }
+        public String toString() { return this.fullname; }
+        //#endregion
     }   
 }
