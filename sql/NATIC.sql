e8c08983434fdcdfe00bf8eb1c8290c950b3e3de
84436 <thien.bui.84436@gmail.com>
2020-12-04 19:17:54 +0700

    [*] sql format

---
+++ sql/NATIC.sql
@@ -0,0 +1,82 @@
+-- WARNING: DESTRUCTIVE ACTION AHEAD
+DROP DATABASE IF EXISTS NATIC;
+
+CREATE DATABASE NATIC;
+USE NATIC;
+
+CREATE TABLE IDGen_States (
+    Prefix  CHAR(2) PRIMARY KEY,
+    State   INT4
+);
+
+CREATE TABLE Accounts (
+    ID      CHAR(10) PRIMARY KEY,
+    Name    VARCHAR(255),
+    Email   VARCHAR(255),
+    Phone   VARCHAR(10),
+    Type    INT1
+);
+
+CREATE TABLE Customers (
+    ID          CHAR(10) PRIMARY KEY,
+    DoB         DATE,
+    Address     VARCHAR(255),
+    SignUpDate  DATE,
+    BookListID  CHAR(10)
+);
+
+CREATE TABLE Staff (
+    ID          CHAR(10) PRIMARY KEY,
+    BranchID    CHAR(10)
+);
+
+CREATE TABLE Branches (
+    ID          CHAR(10) PRIMARY KEY,
+    Name        VARCHAR(255),
+    Address     VARCHAR(255),
+    BookListID  CHAR(10)
+);
+
+CREATE TABLE Books (
+    ISBN        CHAR(13) PRIMARY KEY,
+    BookID      CHAR(10),
+    VersionID   INT1,
+    Title       VARCHAR(255),
+    Author      VARCHAR(255),
+    BookYear    DATE,
+    Publisher   VARCHAR(255),
+    Genre       INT1,
+    Rating      FLOAT,
+    BookFormat  INT1,
+    Price       FLOAT
+);
+
+CREATE TABLE BranchStockLists (
+    ListID  CHAR(10),
+    ISBN    CHAR(13),
+    Stock   INT4
+);
+
+CREATE TABLE CustomerLibraries (
+    ListID      CHAR(10),
+    ISBN        CHAR(13),
+    ExpiryDate  DATE
+);
+
+CREATE TABLE Receipts (
+    ID          CHAR(10) PRIMARY KEY,
+    ISBN        CHAR(13),
+    StaffID     CHAR(10),
+    BranchID    CHAR(10),
+    ReceiptDate DATE,
+    Price       FLOAT,
+    ReturnOn    DATE
+);
+
+CREATE TABLE Reviews (
+    ID          CHAR(10) PRIMARY KEY,
+    CustomerID  CHAR(10),
+    ISBN        CHAR(13),
+    ReviewScore FLOAT,
+    ReviewText  VARCHAR(255)
+);
---
+++ sql/NATIC_DESCRIBE.sql
@@ -0,0 +1,13 @@
+USE NATIC;
+
+-- Show columns, data types, keys and extras
+DESCRIBE IDGen_States;
+DESCRIBE Accounts;
+DESCRIBE Customers;
+DESCRIBE Staff;
+DESCRIBE Branches;
+DESCRIBE Books;
+DESCRIBE BranchStockLists;
+DESCRIBE CustomerLibraries;
+DESCRIBE Receipts;
+DESCRIBE Reviews;
