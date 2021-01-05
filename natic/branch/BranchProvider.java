5cf9285ad2372edcda52dfa68ae49ebbf907308b
Hưng Cao <59307814+HiraiMomo-TWICE@users.noreply.github.com>
2021-01-05 11:10:12 +0700

    Update BranchProvider.java

--- natic/branch/BranchProvider.java
+++ natic/branch/BranchProvider.java
@@ -1,6 +1,7 @@
 package natic.branch;
 
 import natic.IDGenerator;
+import natic.Log;
 import natic.Provider;
 import java.sql.*;
 import java.util.ArrayList;
@@ -20,11 +21,61 @@ public class BranchProvider implements Provider<Branch> {
     }
 
     public void add(Branch o) {
+        try {
+            String query = String.join("\n",
+                "INSERTS INTO BRANCHES",
+                "(ID, Name, Address)",
+                String.format(
+                    "VALUES (\"%s\", \"%s\", \"%s\")",
+                    o.getID(), 
+                    o.getName(), 
+                    o.getAddress()
+                )
+            );
+            Statement stmt = conn.createStatement();
+            stmt.executeQuery(query);
+            Log.l.info(String.format("%s: insert into BRANCHES", o.getID()));
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void edit(Branch o) {
+        try {
+            String query = String.join("\n",
+                "UPDATE BRANCHES",
+                "SET",
+                String.format(
+                    "Name = \"%s\", Address = \"%s\"", 
+                    o.getName(),
+                    o.getAddress()
+                ),
+                "WHERE",
+                String.format("ID = %s", o.getID())
+            );
+            Statement stmt = conn.createStatement();
+            stmt.executeQuery(query);
+            Log.l.info(String.format("%s: update in BRANCHES", o.getID()));
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     
     public void remove(Branch o) {
+        try {
+            String query = String.join("\n",
+                "DELETE FROM BRANCHES",
+                "WHERE",
+                String.format("ID = %s", o.getID())
+            );
+            Statement stmt = conn.createStatement();
+            stmt.executeQuery(query);
+            Log.l.info(String.format("%s: deleted from BRANCHES", o.getID()));
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
